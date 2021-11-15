68b5b71f69b34ce3fe074f3fd885585b58a961c7
Justin Bedford <jbedford94@gmail.com>
2021-11-14 21:59:47 -0500

    user model

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    userName: {
+      type: String,
+      unique: true,
+      required:true,
+      trim: true,
+
+    },
+    emailBy: {
+        type: String,
+        validate: {
+          validator: async function(email) {
+            const user = await this.constructor.findOne({ email });
+            if(user) {
+              if(this.id === user.id) {
+                return true;
+              }
+              return false;
+            }
+            return true;
+          },
+          message: props => 'The specified email address is already in use.'
+        },
+        required: [true, 'User email required']       
+},
+
+    thoughts: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Thought'
+        }
+      ],
+      friends: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'User'
+        }
+      ]
+    },
+
+  );
