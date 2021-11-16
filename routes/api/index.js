dd5f7a09874abdaadd6f2a6e62f817e354ba3cdb
Justin Bedford <jbedford94@gmail.com>
2021-11-15 22:35:06 -0500

    routes fixed

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,6 +4,6 @@ const thoughtRoutes = require('./thought-routes')
 
 // add prefix of `/user` to routes created in `user-routes.js`
 router.use('/user', userRoutes);
-router.use('/thoughts', thoughtRoutes);
+router.use('/thought', thoughtRoutes);
 
 module.exports = router;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,20 +1,20 @@
 const router = require('express').Router();
 
 const {
-    getAllUser,
     getUserById,
     createUser,
     updateUser,
-    deleteUser
+    deleteUser,
+    getAllUsers
   } = require('../../controllers/user-controller');
 
-// Set up GET all and POST at /api/user
+//GET all and POST at /api/user
 router
   .route('/')
-  .get(getAllUser)
+  .get(getAllUsers)
   .post(createUser);
 
-// Set up GET one, PUT, and DELETE at /api/user/:id
+//GET one, PUT, and DELETE at /api/user/:id
 router
   .route('/:id')
   .get(getUserById)
